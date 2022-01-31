394f02bee662ef235a545200ed6205d869c7634a
Stephanie Parma <Sparma01@gmail.com>
2022-01-31 10:16:01 -0500

    done

---
+++ Hello.java
@@ -0,0 +1,18 @@
+class Hello {
+   public static void main(String[] args) {
+      System.out.println("Hello World.");
+      
+      String myText = new String("Hello");
+      String myName = new String("Stephanie");
+      
+      System.out.println(myText + " " + myName + ".");
+
+      int topics = 15;
+
+      System.out.println("This course has " + topics + " modules.");
+      System.out.println("We will learn "+ topics + " different topics.");
+
+   
+   }
+}
+      
