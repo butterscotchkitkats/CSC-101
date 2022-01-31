f9d98bc2bae485fdd340e593b7a01c7369fd30ad
Stephanie Parma <Sparma01@gmail.com>
2022-01-31 10:14:57 -0500

    done

--- 1-24 Module 1.java
+++ 1-24 Module 1.java
@@ -1,10 +1,19 @@
 class Practice {
     public static void main(String[] args){  // When a class is created, this line is needed
+        System.out.print("Hello World."); // Leaves cursor after printed
+        System.out.println("Hello World."); // Prints and then skips cursor to next line
     
     }
 }
 
 
-/*
-   - underscores are permitted, but are not part of Java standard convention
+/* 
+
+- underscores are permitted, but are not part of Java standard convention 
+
+"Hello" + 123 = Hello123
+"Hello" + 123 + 123 = Hello123123
+123 + 123 + "Hello" = 246Hello
+"Hello" + (123 + 123) = Hello246
+
 */
